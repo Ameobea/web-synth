c11ea1309ef99d92da95d7ebda3b6f6c4df2707b
Casey Primozic <me@ameo.link>
2018-10-13 01:06:29 -0500

    Set up mechanics for drawing notes
     * Fix the CSS for the SVG canvases, properly handling scrolling without scrolling the whole body
     * Register callbacks for the functions from mouse and scroll events
     * Create helper functions wrapping the render functions and state containing the most recently element
       * The most recently created element is kept in a variable and can be mutated via getter+setter functions for its attributes.  This allows for things like changing the y/width of the note currently being drawn, for example.
     * The next steps here are keeping some additional state for where the notes are drawn, mapping them to the notes and allowing for them to be selected/deleted.  We can then hook up the notes to a synth and handle playing them.

--- engine/src/lib.rs
+++ engine/src/lib.rs
@@ -1,12 +1,15 @@
 extern crate common;
 extern crate wasm_bindgen;
 
+use common::log;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "./index")]
 extern "C" {
     pub fn render_quad(canvas_index: usize, x: f32, y: f32, width: f32, height: f32, class: &str);
     pub fn render_line(canvas_index: usize, x1: f32, y1: f32, x2: f32, y2: f32, class: &str);
+    // pub fn get_active_attr(key: &str) -> Option<&str>;
+    pub fn set_active_attr(key: &str, val: &str);
 }
 
 /// Height of one of the lines rendered in the grid
@@ -14,6 +17,7 @@ const LINE_HEIGHT: usize = 12;
 const NOTES_PER_OCTAVE: usize = 12; // A,Bb,B,C,C#,D,Eb,E,F,F#,G,Ab
 const OCTAVES: usize = 5;
 const LINE_COUNT: usize = OCTAVES * NOTES_PER_OCTAVE;
+const LINE_BORDER_WIDTH: usize = 1;
 const GRID_HEIGHT: usize = LINE_COUNT * LINE_HEIGHT;
 /// How long one beat is in pixels
 const BEAT_LENGTH_PX: f32 = 20.0;
@@ -23,6 +27,9 @@ const MEASURE_WIDTH_PX: f32 = BEATS_PER_MEASURE as f32 * BEAT_LENGTH_PX;
 const GRID_WIDTH: usize = MEASURE_COUNT * (MEASURE_WIDTH_PX as usize);
 const BG_CANVAS_IX: usize = 0;
 const FG_CANVAS_IX: usize = 1;
+
+static mut MOUSE_DOWN: bool = false;
+static mut MOUSE_DOWN_COORDS: (usize, usize) = (0, 0);
 
 #[wasm_bindgen]
 pub enum Note {
@@ -51,7 +58,7 @@ fn draw_grid_line(y: usize) {
     render_quad(
         BG_CANVAS_IX,
         0.0,
-        (y * LINE_HEIGHT) as f32,
+        (y * (LINE_HEIGHT + LINE_BORDER_WIDTH)) as f32,
         GRID_WIDTH as f32,
         LINE_HEIGHT as f32,
         class,
@@ -73,6 +80,11 @@ fn draw_measure_lines() {
     }
 }
 
+#[inline(always)]
+fn get_line_index(y: usize) -> usize {
+    (y as f32 / ((LINE_HEIGHT + LINE_BORDER_WIDTH) as f32)).trunc() as usize
+}
+
 #[wasm_bindgen]
 pub fn draw_note(note: Note, octave: usize, start_beat: f32, end_beat: f32) {
     let note_line_ix = LINE_COUNT - ((octave * NOTES_PER_OCTAVE) + (note as usize));
@@ -81,12 +93,51 @@ pub fn draw_note(note: Note, octave: usize, start_beat: f32, end_beat: f32) {
     render_quad(
         FG_CANVAS_IX,
         start_x,
-        (note_line_ix * LINE_HEIGHT) as f32,
+        (note_line_ix * (LINE_HEIGHT + LINE_BORDER_WIDTH)) as f32,
         width,
         LINE_HEIGHT as f32,
         "note",
     );
 }
+
+#[wasm_bindgen]
+pub fn handle_mouse_down(x: usize, y: usize) {
+    unsafe {
+        MOUSE_DOWN = true;
+        MOUSE_DOWN_COORDS = (x, y);
+    }
+
+    let line_ix = get_line_index(y);
+    render_quad(
+        FG_CANVAS_IX,
+        x as f32,
+        line_ix as f32 * (LINE_HEIGHT + LINE_BORDER_WIDTH) as f32,
+        0.0,
+        LINE_HEIGHT as f32,
+        "note",
+    );
+}
+
+#[wasm_bindgen]
+pub fn handle_mouse_up(x: usize, y: usize) {
+    unsafe { MOUSE_DOWN = false };
+}
+
+#[wasm_bindgen]
+pub fn handle_mouse_move(x: usize, y: usize) {
+    if !unsafe { MOUSE_DOWN } {
+        return;
+    }
+
+    let down_x = unsafe { MOUSE_DOWN_COORDS.0 };
+    let (minx, maxx) = if x < down_x { (x, down_x) } else { (down_x, x) };
+    let width = maxx - minx;
+    set_active_attr("x", &minx.to_string());
+    set_active_attr("width", &width.to_string());
+}
+
+#[wasm_bindgen]
+pub fn handle_mouse_wheel(_ydiff: isize) {}
 
 #[wasm_bindgen]
 pub fn init() {
--- src/index.tsx
+++ src/index.tsx
@@ -10,59 +10,71 @@ const SVGS: HTMLElement[] = ['background-svg', 'foreground-svg'].map(
   document.getElementById.bind(document)
 ) as any[];
 
-export const render_triangle = (
-  canvas_index: number,
-  x1: number,
-  y1: number,
-  x2: number,
-  y2: number,
-  x3: number,
-  y3: number,
-  color: string,
-  border_color: string
+let ACTIVE_SHAPE: SVGElement = null!;
+
+export const get_active_attr = (key: string): string | null => ACTIVE_SHAPE.getAttribute(key);
+
+/**
+ * Sets an attribute on the active shape to the provided value
+ */
+export const set_active_attr = (key: string, val: string) => ACTIVE_SHAPE.setAttribute(key, val);
+
+const renderHelper = (fn: (...args) => { name: string; attrs: { [key: string]: string } }) => (
+  canvasIndex,
+  ...args
 ) => {
-  const SVG = SVGS[canvas_index];
-  const poly = document.createElementNS('http://www.w3.org/2000/svg', 'polygon');
-  poly.setAttribute('points', `${x1},${y1} ${x2},${y2} ${x3},${y3}`);
-  poly.setAttribute('style', `fill:${color};stroke:${border_color};stroke-width:1`);
-  SVG.appendChild(poly);
+  const svg = SVGS[canvasIndex];
+  const { name, attrs } = fn(...args);
+  const shape = document.createElementNS('http://www.w3.org/2000/svg', name);
+  Object.entries(attrs).forEach(([key, val]) => shape.setAttribute(key, val));
+  svg.appendChild(shape);
+  ACTIVE_SHAPE = shape;
 };
 
-export const render_quad = (
-  canvas_index: number,
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-  className: string
-) => {
-  const SVG = SVGS[canvas_index];
-  const rect = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-  rect.setAttribute('x', x.toString());
-  rect.setAttribute('y', y.toString());
-  rect.setAttribute('width', width.toString());
-  rect.setAttribute('height', height.toString());
-  rect.setAttribute('class', className);
-  SVG.appendChild(rect);
-};
+export const render_triangle = renderHelper(
+  (
+    x1: number,
+    y1: number,
+    x2: number,
+    y2: number,
+    x3: number,
+    y3: number,
+    color: string,
+    border_color: string
+  ) => ({
+    name: 'polygon',
+    attrs: {
+      points: `${x1},${y1} ${x2},${y2} ${x3},${y3}`,
+      style: `fill:${color};stroke:${border_color};stroke-width:1`,
+    },
+  })
+);
 
-export const render_line = (
-  canvas_index: number,
-  x1: number,
-  y1: number,
-  x2: number,
-  y2: number,
-  className: string
-) => {
-  const SVG = SVGS[canvas_index];
-  const line = document.createElementNS('http://www.w3.org/2000/svg', 'line');
-  line.setAttribute('x1', x1.toString());
-  line.setAttribute('y1', y1.toString());
-  line.setAttribute('x2', x2.toString());
-  line.setAttribute('y2', y2.toString());
-  line.setAttribute('class', className);
-  SVG.appendChild(line);
-};
+export const render_quad = renderHelper(
+  (x: number, y: number, width: number, height: number, className: string) => ({
+    name: 'rect',
+    attrs: {
+      x: x.toString(),
+      y: y.toString(),
+      width: width.toString(),
+      height: height.toString(),
+      class: className,
+    },
+  })
+);
+
+export const render_line = renderHelper(
+  (x1: number, y1: number, x2: number, y2: number, className: string) => ({
+    name: 'line',
+    attrs: {
+      x1: x1.toString(),
+      y1: y1.toString(),
+      x2: x2.toString(),
+      y2: y2.toString(),
+      class: className,
+    },
+  })
+);
 
 const deleteAllChildren = (node: HTMLElement) => {
   while (node.firstChild) {
@@ -72,6 +84,19 @@ const deleteAllChildren = (node: HTMLElement) => {
 
 wasm.then(engine => {
   engine.init();
+
+  const scrollOffset = () => document.getElementById('canvases')!.scrollTop - 4;
+  const foregroundCanvas = SVGS[1];
+  foregroundCanvas.addEventListener('mousedown', evt =>
+    engine.handle_mouse_down(evt.pageX, evt.pageY + scrollOffset())
+  );
+  foregroundCanvas.addEventListener('mouseup', evt =>
+    engine.handle_mouse_up(evt.pageX, evt.pageY + scrollOffset())
+  );
+  foregroundCanvas.addEventListener('mousemove', evt =>
+    engine.handle_mouse_move(evt.pageX, evt.pageY + scrollOffset())
+  );
+  foregroundCanvas.addEventListener('wheel', evt => engine.handle_mouse_wheel(evt.deltaX));
 
   engine.draw_note(engine.Note.Cs, 2, 12.0, 16.0);
 });
