c33bc502a36f1a71d0ca6616ba04a6bd51ec1aa0
Casey Primozic <me@ameo.link>
2019-02-22 22:42:37 -0800

    Add Stackline to work experience
     * Add comments to updated `visit()` function in triangles code

--- build_all.sh
+++ build_all.sh
@@ -1,6 +1,6 @@
 cd engine \
   && ./release.sh \
-  && wasm-bindgen ./target/wasm32-unknown-unknown/release/*.wasm --out-dir ./build
+  && wasm-bindgen ./target/wasm32-unknown-unknown/release/*.wasm --browser --remove-producers-section --out-dir ./build
 cd -
 cp ./engine/build/* ./src
 yarn build || npm build
