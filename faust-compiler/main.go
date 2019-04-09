c4530cf5db8dc87d7a6d460f1f3bfab0c21e12e1
Casey Primozic <me@ameo.link>
2019-04-09 02:18:31 -0700

    Hook up frontend to faust compiler
     * Creates a POST request with the current content of the editor
     * Returns a Wasm file containing the compiled output
     * Compiles the Wasm blob into a module and instantiates it

--- faust-compiler/main.go
+++ faust-compiler/main.go
@@ -8,19 +8,24 @@ import (
 	"os/exec"
 )
 
-func compile(tempFilePath string) (wasmOutput []byte, err error) {
-	out, err := exec.Command("faust", "-lang", "wasm", tempFilePath).Output()
+func compile(srcFilePath string, dstFilePath string) (err error) {
+	_, err = exec.Command("faust", "-lang", "wasm", "-o", dstFilePath, srcFilePath).Output()
 
 	if err != nil {
-		return []byte(""), err
+		return err
 	}
 
-	return out, nil
+	return nil
 }
 
 func handleCompile(resWriter http.ResponseWriter, req *http.Request) {
+	resWriter.Header().Set("Access-Control-Allow-Origin", "*")
+
 	if req.Method == "POST" {
 		req.ParseMultipartForm(10e8)
+	} else if req.Method == "HEAD" {
+		resWriter.WriteHeader(200)
+		return
 	} else {
 		resWriter.WriteHeader(405)
 		resWriter.Write([]byte("You must send a POST request."))
@@ -36,8 +41,16 @@ func handleCompile(resWriter http.ResponseWriter, req *http.Request) {
 
 	defer uploadedFile.Close()
 
-	// Create a temporary file
-	tmpfile, err := ioutil.TempFile("", "faust-code")
+	// Create a temporary file for the input source code
+	srctmpfile, err := ioutil.TempFile("", "faust-code")
+	if err != nil {
+		fmt.Println(err)
+		http.Error(resWriter, "Error creating temporary file", 500)
+		return
+	}
+
+	// Create a temporary file for the output Wasm file
+	dsttempfile, err := ioutil.TempFile("", "wasm-output")
 	if err != nil {
 		fmt.Println(err)
 		http.Error(resWriter, "Error creating temporary file", 500)
@@ -45,10 +58,10 @@ func handleCompile(resWriter http.ResponseWriter, req *http.Request) {
 	}
 
 	// Copy the uploaded file to the tempfile
-	io.Copy(tmpfile, uploadedFile)
+	io.Copy(srctmpfile, uploadedFile)
 
 	// Compile the tempfile into WebAssembly
-	wasmFileContent, err := compile(tmpfile.Name())
+	err = compile(srctmpfile.Name(), dsttempfile.Name())
 
 	if err != nil {
 		resWriter.WriteHeader(400)
@@ -56,8 +69,8 @@ func handleCompile(resWriter http.ResponseWriter, req *http.Request) {
 		return
 	}
 
-	resWriter.WriteHeader(200)
-	resWriter.Write(wasmFileContent)
+	// Send the file back to the user
+	http.ServeFile(resWriter, req, dsttempfile.Name())
 }
 
 func main() {
--- src/faustEditor/FaustEditor.tsx
+++ src/faustEditor/FaustEditor.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import ace from 'ace-builds';
 // tslint:disable-next-line:no-submodule-imports
 import 'ace-builds/webpack-resolver';
@@ -11,18 +11,51 @@ interface FaustEditorProps {
   onChange: (newState: string) => void;
 }
 
-const mkFaustEditor = (initialContent: string = '') => ({ onChange }: FaustEditorProps) => (
-  <Suspense fallback={<span>Loading...</span>}>
-    <ReactAce
-      theme='twilight'
-      mode='text'
-      showPrintMargin={false}
-      onChange={onChange}
-      name='ace-editor'
-      width='40vw'
-      value={initialContent}
-    />
-  </Suspense>
-);
+const mkFaustEditor = (initialContent: string = '') => ({ onChange }: FaustEditorProps) => {
+  const [value, setValue] = useState(initialContent);
+
+  return (
+    <Suspense fallback={<span>Loading...</span>}>
+      <ReactAce
+        theme='twilight'
+        mode='text'
+        showPrintMargin={false}
+        onChange={newValue => {
+          onChange(newValue);
+          setValue(newValue);
+        }}
+        name='ace-editor'
+        width='40vw'
+        value={value}
+      />
+
+      <button
+        onClick={async () => {
+          const formData = new FormData();
+          formData.append('code.faust', new Blob([value], { type: 'text/plain' }));
+
+          const res = await fetch('http://localhost:4565/compile', {
+            method: 'POST',
+            // headers: { 'Content-Type': 'multipart/form-data' },
+            body: formData,
+          });
+          const blob = await res.blob();
+
+          // Convert the blob into an `ArrayBuffer` and then compile that into a WebAssembly module
+          const fileReader = new FileReader();
+          fileReader.onload = async evt => {
+            const arrayBuffer: ArrayBuffer = evt.target!.result;
+            const compiledModule = await WebAssembly.compile(arrayBuffer);
+            const wasmInstance = new WebAssembly.Instance(compiledModule);
+            console.log(wasmInstance);
+          };
+          fileReader.readAsArrayBuffer(blob);
+        }}
+      >
+        Compile
+      </button>
+    </Suspense>
+  );
+};
 
 export default mkFaustEditor;
