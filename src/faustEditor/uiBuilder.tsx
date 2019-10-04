264a37cd2aed46565beabb8aa15a785e09b372a1
Casey Primozic <cprimozic@stackline.com>
2019-10-03 17:50:32 -0700

    Add support for buttons in Faust UI builder
     * Uses the two new params added to `react-control-panel` to accomplish it

--- src/faustEditor/uiBuilder.tsx
+++ src/faustEditor/uiBuilder.tsx
@@ -18,6 +18,7 @@ enum InputType {
 enum GroupType {
   vgroup = 'vgroup',
   hgroup = 'hgroup',
+  button = 'button',
 }
 
 interface UiInput extends BaseUiDef {
@@ -41,7 +42,10 @@ type TypeForInputType<K extends UiDef['type']> = Extract<
   { type: K extends InputType ? InputType : K extends GroupType ? GroupType : never }
 >;
 
-const buildControlPanelField = (def: UiDef): ({ [key: string]: any } | null)[] => {
+const buildControlPanelField = (
+  def: UiDef,
+  setParamValue: FaustModuleInstance['setParamValue']
+): ({ [key: string]: any } | null)[] => {
   const mapperFunctions: {
     [K in UiDef['type']]: (
       def: TypeForInputType<K>
@@ -62,15 +66,21 @@ const buildControlPanelField = (def: UiDef): ({ [key: string]: any } | null)[] =
     },
     // TODO: Add label once `react-control-panel` supports that
     [GroupType.vgroup]: ({ items }: { label: string; items: UiDef[] }) =>
-      R.flatten(items.map(buildControlPanelField)),
+      R.flatten(items.map(item => buildControlPanelField(item, setParamValue))),
     // TODO: Add label once `react-control-panel` supports that
     [GroupType.hgroup]: ({ items }: { label: string; items: UiDef[] }) =>
-      R.flatten(items.map(buildControlPanelField)),
+      R.flatten(items.map(item => buildControlPanelField(item, setParamValue))),
+    [GroupType.button]: ({ address }) => ({
+      type: 'button',
+      label: address,
+      onmousedown: () => setParamValue(address, 1),
+      onmouseup: () => setParamValue(address, 0),
+    }),
   };
 
-  const mapper = mapperFunctions[def.type] as (
-    def: UiDef
-  ) => ReturnType<ValueOf<typeof mapperFunctions>>;
+  const mapper = mapperFunctions[def.type] as
+    | ((def: UiDef) => ReturnType<ValueOf<typeof mapperFunctions>>)
+    | undefined;
   if (!mapper) {
     console.warn(`Unable to build UI field of type ${def.type}`);
     return [null];
@@ -86,14 +96,19 @@ const buildControlPanelField = (def: UiDef): ({ [key: string]: any } | null)[] =
     : [mappedDefs as { [key: string]: any }];
 };
 
-const mapUiGroupToControlPanelFields = (group: UiGroup): {}[] =>
-  filterNils(R.flatten(group.items.map(buildControlPanelField)));
+const mapUiGroupToControlPanelFields = (
+  group: UiGroup,
+  setParamValue: FaustModuleInstance['setParamValue']
+): {}[] =>
+  filterNils(R.flatten(group.items.map(item => buildControlPanelField(item, setParamValue))));
 
 const buildControlPanel = (
   uiDef: UiGroup[],
   setParamValue: FaustModuleInstance['setParamValue']
 ) => {
-  const controlPanelFieldDefinitions = R.flatten(uiDef.map(mapUiGroupToControlPanelFields));
+  const controlPanelFieldDefinitions = R.flatten(
+    uiDef.map(item => mapUiGroupToControlPanelFields(item, setParamValue))
+  );
 
   if (R.isEmpty(controlPanelFieldDefinitions)) {
     return null;
