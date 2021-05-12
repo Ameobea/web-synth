99cc63b5b767918282a99b4d8fcecdb4cd694f07
Casey Primozic <me@ameo.link>
2021-05-11 23:14:31 -0700

    Add missing files

---
+++ src/midiEditor/NoteBox/MIDINoteBox.ts
@@ -0,0 +1,129 @@
+import * as PIXI from 'pixi.js';
+
+import type { Note } from 'src/midiEditor/MIDIEditorUIInstance';
+import { NoteBox } from 'src/midiEditor/NoteBox/NoteBox';
+import type NoteLine from 'src/midiEditor/NoteLine';
+import * as conf from '../conf';
+
+export enum NoteDragHandleSide {
+  Left,
+  Right,
+}
+
+export class NoteDragHandle {
+  private parentNote: NoteBox;
+  private graphics: PIXI.Graphics;
+  private side: NoteDragHandleSide;
+
+  constructor(parentNote: NoteBox, side: NoteDragHandleSide) {
+    this.parentNote = parentNote;
+    this.side = side;
+    this.graphics = this.buildInitialGraphics();
+
+    this.render();
+  }
+
+  private buildInitialGraphics(): PIXI.Graphics {
+    const g = new PIXI.Graphics();
+    g.beginFill(0x585858, 0.2);
+    g.drawRect(0, 1, 20, conf.LINE_HEIGHT - 2);
+    g.endFill();
+    this.parentNote.graphics.addChild(g);
+    g.interactive = true;
+    g.cursor = 'ew-resize';
+    g.on('pointerdown', (evt: PIXI.InteractionEvent) => {
+      if (evt.data.button !== 0 || this.parentNote.line.app.selectionBoxButtonDown) {
+        return;
+      }
+
+      const isSelected = this.parentNote.line.app.selectedNoteIDs.has(this.parentNote.note.id);
+      if (!isSelected) {
+        this.parentNote.line.app.selectNote(this.parentNote.note.id);
+      }
+
+      this.parentNote.line.app.startResizingSelectedNotes(evt.data, this.side);
+      evt.stopPropagation();
+    });
+    return g;
+  }
+
+  private computeWidth() {
+    const noteWidth = this.parentNote.getWidthPx();
+    if (noteWidth >= 20) {
+      return 8;
+    }
+
+    return Math.max(Math.floor((noteWidth - 6) / 2), 3);
+  }
+
+  public handleDrag(downPos: PIXI.Point, newPos: PIXI.Point, originalPosBeats: number) {
+    const diffPx = newPos.x - downPos.x;
+    const diffBeats = this.parentNote.line.app.pxToBeats(diffPx);
+    const newPosBeats = this.parentNote.line.app.snapBeat(originalPosBeats + diffBeats);
+    const newLength =
+      this.side === NoteDragHandleSide.Left
+        ? this.parentNote.note.startPoint + this.parentNote.note.length - newPosBeats
+        : newPosBeats - this.parentNote.note.startPoint;
+    if (newLength <= 0) {
+      return;
+    }
+
+    if (this.side === NoteDragHandleSide.Left) {
+      this.parentNote.line.app.resizeNoteHorizontalStart(
+        this.parentNote.line.index,
+        this.parentNote.note.startPoint,
+        this.parentNote.note.id,
+        newPosBeats
+      );
+    } else {
+      this.parentNote.line.app.resizeNoteHorizontalEnd(
+        this.parentNote.line.index,
+        this.parentNote.note.startPoint,
+        this.parentNote.note.id,
+        newPosBeats
+      );
+    }
+  }
+
+  public render() {
+    const parentNoteWidthPx = this.parentNote.getWidthPx();
+    this.graphics!.x =
+      this.side === NoteDragHandleSide.Left
+        ? 0
+        : Math.max(parentNoteWidthPx - this.computeWidth(), 0);
+    this.graphics.scale = new PIXI.Point(this.computeWidth() / 20, 1);
+  }
+
+  public destroy() {
+    if (this.graphics) {
+      this.parentNote.graphics.removeChild(this.graphics);
+      this.graphics.destroy();
+    }
+  }
+}
+
+export default class MIDINoteBox extends NoteBox {
+  public leftDragHandle: NoteDragHandle;
+  public rightDragHandle: NoteDragHandle;
+
+  constructor(line: NoteLine, note: Note) {
+    super(line, note);
+
+    this.leftDragHandle = new NoteDragHandle(this, NoteDragHandleSide.Left);
+    this.rightDragHandle = new NoteDragHandle(this, NoteDragHandleSide.Right);
+  }
+
+  public render() {
+    super.render();
+
+    this.leftDragHandle.render();
+    this.rightDragHandle.render();
+  }
+
+  public destory() {
+    super.destroy();
+
+    this.leftDragHandle.destroy();
+    this.rightDragHandle.destroy();
+  }
+}
---
+++ src/midiEditor/NoteBox/SampleEditorNoteBox.ts
@@ -0,0 +1,64 @@
+import type { Note } from 'src/midiEditor/MIDIEditorUIInstance';
+import { NoteBox } from 'src/midiEditor/NoteBox/NoteBox';
+import type NoteLine from 'src/midiEditor/NoteLine';
+import * as conf from 'src/midiEditor/conf';
+
+class SampleEditorNoteBoxLabel {
+  private noteBox: NoteBox;
+  private text: PIXI.Text;
+  private background: PIXI.Graphics;
+
+  constructor(noteBox: NoteBox, labelText: string, widthPx: number) {
+    this.noteBox = noteBox;
+    this.text = new PIXI.Text(labelText, {
+      fontFamily: 'PT Sans',
+      fontSize: conf.SAMPLE_EDITOR_LABEL_FONT_SIZE,
+      fill: conf.SAMPLE_EDITOR_LABEL_TEXT_COLOR,
+      align: 'left',
+    });
+    this.background = new PIXI.Graphics()
+      .beginFill(conf.SAMPLE_EDITOR_LABEL_BACKGROUND_COLOR)
+      .lineStyle(1, conf.SAMPLE_EDITOR_LABEL_BORDER_COLOR)
+      .drawRect(0, 0, widthPx, conf.SAMPLE_EDITOR_LABEL_HEIGHT);
+
+    this.background.addChild(this.text);
+    noteBox.graphics.addChild(this.background);
+  }
+
+  public destroy() {
+    this.noteBox.graphics.removeChild(this.background);
+    this.background.removeChild(this.text);
+    this.text.destroy();
+    this.background.destroy();
+  }
+}
+
+export default class SampleEditorNoteBox extends NoteBox {
+  constructor(line: NoteLine, note: Note) {
+    super(line, note);
+  }
+
+  private lastLabelWidthPx = 0;
+  private label: SampleEditorNoteBoxLabel | undefined;
+
+  public render() {
+    super.render();
+
+    const widthPx = this.line.app.beatsToPx(this.note.length) - 1;
+    const labelWidthPx = widthPx - 8;
+
+    // Perf optimization to avoid re-rendering the label if not necessary
+    if (labelWidthPx === this.lastLabelWidthPx) {
+      return;
+    }
+    this.lastLabelWidthPx = labelWidthPx;
+
+    if (labelWidthPx >= 20) {
+      this.label?.destroy();
+      this.label = new SampleEditorNoteBoxLabel(this, 'TODO TODO TODO', labelWidthPx);
+    } else {
+      this.label?.destroy();
+      this.label = undefined;
+    }
+  }
+}
