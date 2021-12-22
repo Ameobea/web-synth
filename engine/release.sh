d08613146059396030ee24c655c06a0e60e14096
Casey Primozic <me@ameo.link>
2021-12-22 02:17:17 -0600

    Fix build

--- engine/release.sh
+++ engine/release.sh
@@ -21,8 +21,6 @@
 #   cd ../midi_quantizer && cargo build --release --target wasm32-unknown-unknown &&
 #   cd ../quantizer && cargo build --release --target wasm32-unknown-unknown
 
-cargo build --release --target wasm32-unknown-unknown \
-    engine midi polysynth wavetable waveform_renderer granular event_scheduler sidechain noise_gen distortion adsr \
-    note_container sample_editor delay sample_player wav_decoder looper midi_quantizer quantizer &&
+cargo build --release --target wasm32-unknown-unknown --workspace --exclude common --exclude dsp --exclude wbg_logging &&
   mv ./target/wasm32-unknown-unknown/release/wavetable.wasm ./target/wasm32-unknown-unknown/release/wavetable_no_simd.wasm &&
   cd wavetable && RUSTFLAGS="-Ctarget-feature=+simd128" cargo build --target wasm32-unknown-unknown --release --features=simd
