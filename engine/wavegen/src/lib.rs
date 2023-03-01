d3427a703b31578a2d2b729d623c3f853801c7c7
Casey Primozic <me@ameo.link>
2023-02-28 19:30:13 -0800

    Some initial experiments with FFT-based wavetable synthesis

---
+++ engine/wavegen/src/lib.rs
@@ -0,0 +1,4 @@
+//! Utilities for generating waveforms and wavetables using inverse FFT.
+
+#[cfg(test)]
+mod tests;
---
+++ engine/wavegen/src/tests/helpers.rs
@@ -0,0 +1,15 @@
+use textplots::{Chart, Plot, Shape};
+
+pub fn plot_wave(samples: &[f32]) {
+    let points: Vec<(f32, f32)> = samples
+        .iter()
+        .enumerate()
+        .map(|(i, &s)| {
+            let x = i as f32 / samples.len() as f32;
+            (x, s)
+        })
+        .collect();
+    Chart::new(200, 100, 0., 1.)
+        .lineplot(&Shape::Lines(&points))
+        .display();
+}
