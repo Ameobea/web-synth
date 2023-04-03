a2a20407a5be388e39e1535acc4a30df68ff7e5f
Casey Primozic <me@ameo.link>
2023-04-03 16:18:31 -0700

    Fix build

--- engine/release.sh
+++ engine/release.sh
@@ -5,9 +5,9 @@ cargo build --release --target wasm32-unknown-unknown --workspace \
   --exclude granular --exclude vocoder --exclude oscilloscope --exclude spectrum_viz &&
   rm -f ./target/wasm32-unknown-unknown/release/wavetable_no_simd.wasm && mv ./target/wasm32-unknown-unknown/release/wavetable.wasm ./target/wasm32-unknown-unknown/release/wavetable_no_simd.wasm &&
   cd spectrum_viz && cargo build --target wasm32-unknown-unknown --release --no-default-features --features=line_viz &&
-  cargo build --target wasm32-unknown-unknown --release &&
   cd .. && rm -f ./target/wasm32-unknown-unknown/release/spectrum_viz_full.wasm && mv ./target/wasm32-unknown-unknown/release/spectrum_viz.wasm ./target/wasm32-unknown-unknown/release/spectrum_viz_full.wasm &&
-  cd wavegen && cargo build --target wasm32-unknown-unknown --release &&
+  cd spectrum_viz && cargo build --target wasm32-unknown-unknown --release &&
+  cd ../wavegen && cargo build --target wasm32-unknown-unknown --release &&
   cd ../level_detector && cargo build --target wasm32-unknown-unknown --release &&
   cd ../granular && cargo build --target wasm32-unknown-unknown --release &&
   cd ../vocoder && RUSTFLAGS="-Ctarget-feature=+simd128" cargo build --target wasm32-unknown-unknown --release &&
