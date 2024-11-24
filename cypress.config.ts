5ceecb13a3a77c4670795b1583cb52b051ab5d5e
Casey Primozic <me@ameo.link>
2024-11-23 17:39:14 -0800

    Try to fix cypress video recording

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,26 @@
-import { defineConfig } from 'cypress'
+import fs from 'fs';
+
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   projectId: 'gahph7',
   e2e: {
-    setupNodeEvents(on, config) {},
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+    // delete videos for tests without errors
+    setupNodeEvents(on, config) {
+      on('after:spec', (spec: Cypress.Spec, results: CypressCommandLine.RunResult) => {
+        if (results && results.video) {
+          // Do we have failures for any retry attempts?
+          const failures = results.tests.some(test =>
+            test.attempts.some(attempt => attempt.state === 'failed')
+          );
+          if (!failures) {
+            // delete the video if the spec passed and no tests retried
+            fs.unlinkSync(results.video);
+          }
+        }
+      });
+    },
   },
-})
+  video: true,
+});
